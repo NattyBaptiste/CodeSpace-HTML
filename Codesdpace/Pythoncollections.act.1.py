52f476204de93520915ea8bca3bc526680a5e75e
NattyBaptiste <143715202+NattyBaptiste@users.noreply.github.com>
2026-06-10 23:37:38 +0100

    Add files via upload

---
+++ Codesdpace/Pythoncollections.act.1.py
@@ -0,0 +1,8 @@
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Calculate the sum of all values in the list
+total = sum(numbers)
+
+# Display the result
+print("The sum is", total)
