ff2207c33732aac4159c41bf56c305456a9fc1c9
NattyBaptiste <143715202+NattyBaptiste@users.noreply.github.com>
2026-06-10 23:20:25 +0100

    Delete Codesdpace directory

--- Codesdpace/Pythonloops.act.2.py
+++
@@ -1,7 +0,0 @@
-# Input a number from the user
-number = int(input("Input a number: "))
-
-# Loop from 1 to 10 to generate the multiplication table
-for i in range(1, 11):
-    # Print the multiplication result
-    print(f"{number} x {i} = {number * i}")
